29608194139aa3f68ef3beb1e57f288d5965918e
skpdkp <skpdkp8@gmail.com>
2025-09-01 16:54:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -398,7 +398,7 @@ async def start_handler(_, message):
     reply_markup = InlineKeyboardMarkup(buttons)
 
     await message.reply_animation(
-        animation="https://files.catbox.moe/8kc5b3.png",
+        animation="https://files.catbox.moe/ua21yd.jpg",
         caption=caption,
         parse_mode=ParseMode.MARKDOWN,
         reply_markup=reply_markup
